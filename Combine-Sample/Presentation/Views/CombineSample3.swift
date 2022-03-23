59fe66d78f391cf41dd7f9232b3259ccd9db45ab
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-03-23 21:16:44 +0900

    ファイル追加

---
+++ Combine-Sample/Presentation/Views/CombineSample3.swift
@@ -0,0 +1,20 @@
+//
+//  CombineSample3.swift
+//  Combine-Sample
+//
+//  Created by Hiroaki-Hirabayashi on 2022/03/23.
+//
+
+import SwiftUI
+
+struct CombineSample3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CombineSample3_Previews: PreviewProvider {
+    static var previews: some View {
+        CombineSample3()
+    }
+}
