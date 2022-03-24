6a344d57f794dc311d476f9c5e366cfe5179cb33
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-03-24 23:07:26 +0900

    ステージング忘れ

--- Combine-Sample/Utils/PlaceHolderTextEditor.swift
+++ Combine-Sample/Utils/PlaceHolderTextEditor.swift
@@ -5,4 +5,30 @@
 //  Created by Hiroaki-Hirabayashi on 2022/03/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct PlaceHolderTextEditor: View {
+    var placeholderText: String
+    @Binding var text: String
+
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholderText)
+                    .foregroundColor(.black)
+                    .padding(.top, 24)
+                    .padding(.leading, 16)
+            }
+            TextEditor(text: $text)
+                .padding(.top)
+                .frame(maxWidth: .infinity, maxHeight: 251)
+        }
+        .onAppear {
+            UITextView.appearance().backgroundColor = .clear
+        }
+        .onDisappear {
+            UITextView.appearance().backgroundColor = nil
+        }
+    }
+}
+
