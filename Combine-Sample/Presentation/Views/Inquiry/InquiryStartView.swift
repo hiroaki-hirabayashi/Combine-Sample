f175c72ed95f917dca75c19ce7e7afc51fe5f545
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-03-29 21:47:04 +0900

    モーダル遷移View追加

---
+++ Combine-Sample/Presentation/Views/Inquiry/InquiryStartView.swift
@@ -0,0 +1,20 @@
+//
+//  InquiryStartView.swift
+//  Combine-Sample
+//
+//  Created by Hiroaki-Hirabayashi on 2022/03/29.
+//
+
+import SwiftUI
+
+struct InquiryStartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InquiryStartView_Previews: PreviewProvider {
+    static var previews: some View {
+        InquiryStartView()
+    }
+}
---
+++ Combine-Sample/Utils/ModalHostingController.swift
@@ -0,0 +1,8 @@
+//
+//  Modal.swift
+//  Combine-Sample
+//
+//  Created by Hiroaki-Hirabayashi on 2022/03/29.
+//
+
+import Foundation
